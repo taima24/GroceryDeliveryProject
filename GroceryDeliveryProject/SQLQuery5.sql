4e51b92c5d6f249badc165cb86b87711762e8ea1
taima24 <taimaaldarassi@gmail.com>
2025-08-08 19:01:54 +0300

    first commit

---
+++ GroceryDeliveryProject/SQLQuery5.sql
@@ -0,0 +1,23 @@
+﻿alter table Products
+add constraint fk_products_categories
+foreign key (CategoryId) references Categories(Id);
+
+alter table CartItems
+add constraint fk_cartitems_users
+foreign key (UserId) references Users(Id);
+
+alter table CartItems
+add constraint fk_cartitems_products
+foreign key (ProductId) references Products(Id);
+
+alter table Orders
+add constraint fk_orders_users
+foreign key (UserId) references Users(Id);
+
+alter table OrderItems
+add constraint fk_orderitems_orders
+foreign key (OrderId) references Orders(Id);
+
+alter table OrderItems
+add constraint fk_orderitems_Products
+foreign key (ProductId) references Products(Id);
